92d55989434b5893b420477e2fe05b0e26c11fdc
Ирина <vredprep@inbox.ru>
2022-09-21 12:42:49 +0500

    add group in application

--- tests/test_add_group.py
+++ tests/test_add_group.py
@@ -1,2 +1,7 @@
 def test_add_group(app):
-    pass
+    name_gr = "my new group"
+    old_group_list = app.groups.get_group_list()
+    app.groups.add_new_group(name_gr)
+    new_group_list = app.groups.get_group_list()
+    old_group_list.append(name_gr)
+    assert sorted(old_group_list) == sorted(new_group_list)
